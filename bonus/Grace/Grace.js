c4f6ecb14e29a4d7487ddc78c794f07c9d70e2af
titus <titus@dump-titus.42.fr>
2017-09-19 11:59:24 +0200

    [debug] added 3 variables into Grace

--- bonus/Grace/Grace.js
+++ bonus/Grace/Grace.js
@@ -2,6 +2,9 @@ var fs = require('fs');
 /*
   comment
 */
+let x=1;
+let y=2;
+let z=3;
 (function foo() {
-fs.writeFileSync("Grace_kid.js", "var fs = require('fs');\n/*\n  comment\n*/\n(" + foo.toString() + ")();\n");
+fs.writeFileSync("Grace_kid.js", "var fs = require('fs');\n/*\n  comment\n*/\nlet x=1;\nlet y=2;\nlet z=3;\n(" + foo.toString() + ")();\n");
 })();
